128526811e154663a198d15ca65768b92c8c141a
Project0 <OLIVARA5@uci.edu>
2025-12-01 23:57:02 -0800

    Add test comment to noweeds env

--- env/stardew_mine_env_noweeds.py
+++ env/stardew_mine_env_noweeds.py
@@ -4,6 +4,8 @@ import gymnasium as gym
 
 from reward_functions import compute_reward
 
+# NOTE: Temporary test comment to verify git commit flow.
+
 
 class StardewMineEnv_NoWeeds(gym.Env):
     """Grid-based mining environment with NO weeds."""
